1b4f0947439a5d845c8f011245193560955dd8f9
Daniel.xiao <dxweixiao@tencent.com>
2023-05-27 10:45:36 +0800

    fix: safely set state

--- src/hooks/useStateCB.js
+++ src/hooks/useStateCB.js
@@ -3,6 +3,7 @@ import { useState, useEffect, useRef } from 'react';
 export function useStateCB(initialState) {
   const [state, setState] = useState(initialState);
   const stateRef = useRef(initialState);
+  const mountedRef = useRef(false);
 
   useEffect(() => {
     if (setState.callback) {
@@ -11,7 +12,21 @@ export function useStateCB(initialState) {
     }
   }, [state]);
 
+  useEffect(() => {
+    mountedRef.current = true;
+    return function() {
+      mountedRef.current = false;
+    };
+  }, []);
+
   function newSetState(newState, callback) {
+    if (!mountedRef.current) {
+      // To safely set state
+      console.warn(
+        'The react component has been unmounted, and the set state action has been automatically ignored',
+      );
+      return;
+    }
     if (callback) setState.callback = callback;
     stateRef.current = newState;
     setState(newState);
